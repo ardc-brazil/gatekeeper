dd702483555485b3c4a58afd0f5192199cdd4503
Caio Maia <caio.maia@nubank.com.br>
2023-07-20 22:44:20 -0300

    Create indexes for datasets

--- app/models/datasets.py
+++ app/models/datasets.py
@@ -1,6 +1,7 @@
 import sqlalchemy
 from app import db
 from sqlalchemy.dialects.postgresql import UUID, JSONB
+from sqlalchemy import Index
 
 class Datasets(db.Model):
     __tablename__ = 'datasets'
@@ -8,3 +9,6 @@ class Datasets(db.Model):
     name = db.Column(db.String(256), nullable=False)
     data = db.Column(JSONB, nullable=True)
     is_enabled = db.Column(db.Boolean, nullable=False, default=True)
+
+    __table_args__ = (Index('idx_is_enabled', 'is_enabled'),
+                      Index('idx_name', 'name'))
