ddf90281cd4452709493083e2c0c24104c63cced
Caio Maia <caio.maia@nubank.com.br>
2023-07-26 21:07:43 -0300

    Fix for gunicorn

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from app import create_app
 
-app = create_app()
+# app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8000)
+    # app.run(host='localhost', port=8000)
+    create_app = create_app()
+    create_app.run()
+else:
+    gunicorn_app = create_app()
