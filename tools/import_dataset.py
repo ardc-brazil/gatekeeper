2cc8d182b64189090705a1b192227519d51d8f58
Caio Maia <caio.maia@nubank.com.br>
2023-07-26 22:58:38 -0300

    Add code to manually import datasets

---
+++ tools/import_dataset.py
@@ -0,0 +1,14 @@
+import json
+import requests
+
+f = open('datasets.json')
+data = json.load(f)
+for i in data:
+    del i["id"]
+    c = {"name": i["name"]}
+    del i["name"]
+    c["data"] = i
+    r = requests.post('http://ec2-34-194-118-180.compute-1.amazonaws.com:8080/api/datasets/', json=c)
+    print(r.status_code)
+
+f.close()
