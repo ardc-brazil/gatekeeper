f49e15a5616d3d00dea2b75438f07929b0689945
Caio Maia <caio.maia@nubank.com.br>
2023-07-26 21:10:54 -0300

    Rollback fix in favor of command execution

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from app import create_app
 
-# app = create_app()
+app = create_app()
 
 if __name__ == '__main__':
-    # app.run(host='localhost', port=8000)
-    create_app = create_app()
-    create_app.run()
-else:
-    gunicorn_app = create_app()
+    app.run(host='localhost', port=8000)
