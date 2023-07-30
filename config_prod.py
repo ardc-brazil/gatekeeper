897c6bdecd9a20515f9ec2eadccda414ce220957
Caio Maia <caio.maia@nubank.com.br>
2023-07-30 01:11:57 -0300

    Fix prod config database url

--- config_prod.py
+++ config_prod.py
@@ -1,5 +1,5 @@
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'postgresql://gk_admin:WYnAG9!qzhfx7hDatJcs@localhost:5432/gatekeeper_db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://gk_admin:WYnAG9!qzhfx7hDatJcs@gatekeeper-gatekeeper-db-1:5432/gatekeeper_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 MIGRATION_DIR = 'migrations'
 SQLALCHEMY_ECHO = True
