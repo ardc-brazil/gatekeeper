6d506324afda9d86598f303b231190d28850215b
Caio Maia <caio.maia@nubank.com.br>
2023-08-04 00:36:10 -0300

    Create check_password function

--- app/services/secrets.py
+++ app/services/secrets.py
@@ -4,3 +4,6 @@ def hash_password(password):
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password.decode('utf-8')
+
+def check_password(password, hashed_password):
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
