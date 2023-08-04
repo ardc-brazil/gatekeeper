12d71bc41e06991cf148115d1b3758692fec0592
Caio Maia <caio.maia@nubank.com.br>
2023-08-04 00:36:29 -0300

    Adding functions to clients repository

--- app/repositories/clients.py
+++ app/repositories/clients.py
@@ -1,7 +1,17 @@
 from methodtools import lru_cache
 from app.models.clients import Clients
+from app import db
 
 class ClientsRepository:
-    @lru_cache
-    def fetch(self, api_key):
-        return Clients.query.filter_by(key=api_key).first()
+    # @lru_cache
+    def fetch(self, api_key, is_enabled=True):
+        return Clients.query.filter_by(key=api_key, is_enabled=is_enabled).first()
+    
+    def fetch_all(self, is_enabled=True):
+        return Clients.query.filter_by(is_enabled=is_enabled).all()
+    
+    def upsert(self, client):
+        if (client.key is None):
+            db.session.add(client)
+        db.session.commit()
+    
