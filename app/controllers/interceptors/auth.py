093bd0134d5f5efce2491d486f2e4a879a9fd58c
Caio Maia <caio.maia@nubank.com.br>
2023-08-04 00:36:48 -0300

    Fix auth decorator

--- app/controllers/interceptors/auth.py
+++ app/controllers/interceptors/auth.py
@@ -1,25 +1,28 @@
+import logging
 from flask import request, jsonify
 from functools import wraps
 
 from app.repositories.clients import ClientsRepository
-from app.services.secrets import hash_password
+from app.services.clients import ClientsService
+from app.services.secrets import check_password, hash_password
 
-clients_repository = ClientsRepository()
+service = ClientsService()
 
 def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        api_key = request.headers['x-api-token']
-        api_secret = request.headers['x-api-secret']
+        api_key = request.headers.get('X-Api-Key')
+        api_secret = request.headers.get('X-Api-Secret')
 
-        client = clients_repository.fetch(api_key)
+        if (api_key is None or api_secret is None):
+            return jsonify({'message': 'Unauthorized'}), 401
+
+        client = service.fetch(api_key)
         
         if (client is None):
             return jsonify({'message': 'Unauthorized'}), 401
         
-        hashed_key = hash_password(api_secret)
-
-        if (hashed_key != client.secret):
+        if not check_password(api_secret, client['secret']):
             return jsonify({'message': 'Unauthorized'}), 401
 
         return f(*args, **kwargs)
