007997e631c573550424fcdaafa189320bd3a36b
Caio Maia <caio.maia@nubank.com.br>
2023-08-03 23:16:41 -0300

    Rename function

--- app/controllers/infrastructure.py
+++ app/controllers/infrastructure.py
@@ -3,5 +3,5 @@ from flask import Blueprint
 infrastructure_bp = Blueprint('infra', __name__, url_prefix='/infra')
 
 @infrastructure_bp.get('/health-check')
-def flask_health_check():
+def health_check():
 	return "success"
