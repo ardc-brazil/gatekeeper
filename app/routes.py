715d00a9f16f0673a77a421a3c55d6e2ae9075de
Caio Maia <caio.maia@nubank.com.br>
2023-08-06 00:19:01 -0300

    Remove routes file

--- app/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-root = Blueprint('routes', __name__, url_prefix='/api')
-# from app.controllers.datasets import datasets_bp
-# root.register_blueprint(datasets_bp)
-from app.controllers.infrastructure import infrastructure_bp
-root.register_blueprint(infrastructure_bp)
-from app.controllers.v1.clients import clients_bp
-root.register_blueprint(clients_bp)
