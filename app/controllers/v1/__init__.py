6ab14c02922d73cf4bfbf55ffb2d4f025f050c31
Caio Maia <caio.maia@nubank.com.br>
2023-08-06 02:47:55 -0300

    Fix swagger html

--- app/controllers/v1/__init__.py
+++ app/controllers/v1/__init__.py
@@ -1,10 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, url_for
 from flask_restx import Api
 from app.controllers.v1.datasets.datasets import namespace as datasets_ns
 from app.controllers.v1.clients.clients import namespace as clients_ns
 from app.controllers.v1.datasets.datasets_filters import namespace as datasets_filters_ns
 from app.controllers.v1.infrastructure.infrastructure import namespace as healthcheck_ns
 
+class PatchedApi(Api):
+    @property
+    def specs_url(self):
+        return url_for(self.endpoint('specs'))
+    
 api = Blueprint('apiv1', __name__, url_prefix='/api/v1')
 
 authorizations = {
@@ -25,7 +30,7 @@ authorizations = {
     }
 }
 
-api_extension = Api(
+api_extension = PatchedApi(
     api,
     title='Gatekeeper API Gateway',
     version='1.0',
