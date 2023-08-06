a397302993fcced8f80f1445d136167296b05ba6
Caio Maia <caio.maia@nubank.com.br>
2023-08-06 00:52:39 -0300

    Delete dataset search

--- app/controllers/v1/datasets/datasets.py
+++ app/controllers/v1/datasets/datasets.py
@@ -1,39 +1,13 @@
 import logging
-from flask import request, jsonify, make_response
-from app.controllers.interceptors.auth import public_route, requires_auth
+from flask import request
+from app.controllers.interceptors.auth import requires_auth
 from app.services.datasets import DatasetService
-from flask_restx import Namespace, Resource, fields
+from flask_restx import Namespace, Resource
 from werkzeug.exceptions import NotFound, InternalServerError
+from app.controllers.v1.datasets.resources.datasets_models import dataset_model, dataset_create_model
 
 service = DatasetService()
 namespace = Namespace('datasets', 'Dataset operations')
-dataset_model = namespace.model('Dataset', {
-    'id': fields.String(
-        readonly=True,
-        required=True,
-        description='Dataset ID'
-    ),
-    'name': fields.Raw(
-        required=True,
-        description='Dataset name'
-    ),
-    'data': fields.String(
-        required=False,
-        description='Dataset information'
-    ),
-    'is_enabled': fields.Boolean(
-        required=True,
-        description='Dataset status'
-    )
-})
-
-dataset_create_model = namespace.model('DatasetCreate', {
-    'id': fields.String(
-        readonly=True,
-        required=True,
-        description='Dataset ID'
-    )
-})
 
 @namespace.route('/<string:dataset_id>')
 @namespace.param('dataset_id', 'The dataset identifier')
@@ -82,11 +56,27 @@ class DatasetsListController(Resource):
 
     method_decorators = [requires_auth]
     
+    @namespace.doc("Search datasets")
+    @namespace.param('categories', 'Dataset categories, comma separated')
+    @namespace.param('level', 'Dataset level')
+    @namespace.param('data_types', 'Dataset data types, comma separated')
+    @namespace.param('date_from', 'Dataset date from, YYYY-MM-DD')
+    @namespace.param('date_to', 'Dataset date to, YYYY-MM-DD')
+    @namespace.param('full_text', 'Dataset full text')
     @namespace.marshal_list_with(dataset_model)
     def get(self):
         '''Fetch all datasets'''
         try:
-            return service.fetch_all_datasets(), 200
+            query_params = {
+                'categories': request.args.get('categories').split(',') if request.args.get('categories') else [],
+                'level': request.args.get('level'),
+                'data_types': request.args.get('data_types').split(',') if request.args.get('data_types') else [],
+                'date_from': request.args.get('date_from'),
+                'date_to': request.args.get('date_to'),
+                'full_text': request.args.get('full_text')
+            }
+
+            return service.search_datasets(query_params), 200
         except Exception as e:
             logging.error(e)
             raise InternalServerError()
