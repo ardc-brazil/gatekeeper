3ca055964ea78edce902f00fc3bce584851c1975
Caio Maia <caio.maia@nubank.com.br>
2023-08-07 22:28:31 -0300

    Configure casbin in app

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_migrate import Migrate
 import flask_restx
 from flask_sqlalchemy import SQLAlchemy
+from casbin_sqlalchemy_adapter import Adapter as SQLAlchemyAdapter
+from casbin import Enforcer
+
+from app.controllers.interceptors.authorization import authorization_middleware
 
 db = SQLAlchemy()
 
@@ -23,5 +27,11 @@ def create_app():
     ############################################################
     # from app.controllers.v2 import api 
     # app.register_blueprint(api)
+
+    adapter = SQLAlchemyAdapter(db)
+    enforcer = Enforcer('app/resources/casbin_policy.conf', adapter)
+    enforcer.load_policy()
+
+    app.wsgi_app = authorization_middleware(enforcer)(app.wsgi_app)
 
     return app
