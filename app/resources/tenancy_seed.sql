39b31317867f4a87a63dcf68c2561adfbbc604ca
Caio Maia <caaiomaia@gmail.com>
2024-02-16 12:09:14 -0300

    Use hierarchy in tenancy

--- app/resources/tenancy_seed.sql
+++ app/resources/tenancy_seed.sql
@@ -1,2 +1,4 @@
-INSERT INTO tenancies (name, is_enabled) VALUES ('amazon-face', TRUE);
-INSERT INTO tenancies (name, is_enabled) VALUES ('datamap', TRUE)
+INSERT INTO tenancies (name, is_enabled) VALUES ('datamap/production/amazon-face', TRUE);
+INSERT INTO tenancies (name, is_enabled) VALUES ('datamap/staging/amazon-face', TRUE);
+INSERT INTO tenancies (name, is_enabled) VALUES ('datamap/production/data-amazon', TRUE);
+INSERT INTO tenancies (name, is_enabled) VALUES ('datamap/staging/data-amazon', TRUE);
