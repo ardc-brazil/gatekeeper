e22290eadabd57354ba44572b7533f0fa8cd45bb
Caio Maia <caaiomaia@gmail.com>
2024-03-30 21:36:11 -0300

    Fix configs to run docker locally

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'postgresql://gk_admin:WYnAG9!qzhfx7hDatJcs@localhost:5432/gatekeeper_db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://gk_admin:WYnAG9!qzhfx7hDatJcs@datamap_gatekeeper_db:5432/gatekeeper_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 MIGRATION_DIR = 'migrations'
 SQLALCHEMY_ECHO = True
-CASBIN_DATABASE_URL = "postgresql://gk_admin:WYnAG9!qzhfx7hDatJcs@gatekeeper_db:5432/gatekeeper_db"
+CASBIN_DATABASE_URL = "postgresql://gk_admin:WYnAG9!qzhfx7hDatJcs@datamap_gatekeeper_db:5432/gatekeeper_db"
