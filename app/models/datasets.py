66bc9bac016d584e5913e78b557dbb9af375ed3d
Caio Maia <caaiomaia@gmail.com>
2024-04-06 20:21:11 -0300

    Add dataset file and version migration

--- app/models/datasets.py
+++ app/models/datasets.py
@@ -3,6 +3,17 @@ from app import db
 from sqlalchemy.dialects.postgresql import UUID, JSONB
 from sqlalchemy import Index
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+dataset_version_association = db.Table('dataset_version',
+    db.Column('dataset_id', UUID(as_uuid=True), db.ForeignKey('datasets.id'), primary_key=True),
+    db.Column('version_id', UUID(as_uuid=True), db.ForeignKey('dataset_versions.id'), primary_key=True)
+)
+
+dataset_files_association = db.Table('dataset_file',
+    db.Column('dataset_id', UUID(as_uuid=True), db.ForeignKey('datasets.id'), primary_key=True),
+    db.Column('file_id', UUID(as_uuid=True), db.ForeignKey('data_files.id'), primary_key=True)                               
+)
 
 class Datasets(db.Model):
     __tablename__ = 'datasets'
@@ -15,5 +26,36 @@ class Datasets(db.Model):
     owner_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=True)
     tenancy = db.Column(db.String(256), nullable=True)
 
+    versions = relationship('DatasetVersions', secondary=dataset_version_association, backref='users')
+    files = relationship('DataFiles', secondary=dataset_files_association, backref='users')
+
     __table_args__ = (Index('idx_is_enabled', 'is_enabled'),
                       Index('idx_name', 'name'))
+
+class DatasetVersion(db.Model):
+    __tablename__ = 'dataset_versions'
+    id = db.Column(UUID(as_uuid=True), primary_key=True, server_default=sqlalchemy.text("gen_random_uuid()"))
+    name = db.Column(db.String(256), nullable=False)
+    description = db.Column(db.String(256), nullable=True)
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
+    author_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=True)
+
+    __table_args__ = (Index('idx_dataset_versions_name', 'name'),
+                      Index('idx_dataset_versions_created_at', 'created_at'))
+
+class DataFile(db.Model):
+    __tablename__ = 'data_files'
+    id = db.Column(UUID(as_uuid=True), primary_key=True, server_default=sqlalchemy.text("gen_random_uuid()"))
+    name = db.Column(db.String(1024), nullable=False)
+    size_bytes = db.Column(db.Integer, nullable=False)
+    extension = db.Column(db.String(64), nullable=True)
+    format = db.Column(db.String(64), nullable=True)
+    storage_file_name = db.Column(db.String(1024), nullable=True)
+    storage_path = db.Column(db.String(2048), nullable=True)
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
+    author_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=False)
+
+    __table_args__ = (Index('idx_data_files_name', 'name'),
+                      Index('idx_data_files_created_at', 'created_at'))
