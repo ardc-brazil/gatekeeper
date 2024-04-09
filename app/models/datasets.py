db8aa3ad9cf9e3bd675637e410b2d02bdd2c0196
Caio Maia <caaiomaia@gmail.com>
2024-04-08 22:01:55 -0300

    Fix domain for datasets

--- app/models/datasets.py
+++ app/models/datasets.py
@@ -5,16 +5,6 @@ from sqlalchemy import Index
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 
-dataset_version_association = db.Table('dataset_version',
-    db.Column('dataset_id', UUID(as_uuid=True), db.ForeignKey('datasets.id'), primary_key=True),
-    db.Column('version_id', UUID(as_uuid=True), db.ForeignKey('dataset_versions.id'), primary_key=True)
-)
-
-dataset_files_association = db.Table('dataset_file',
-    db.Column('dataset_id', UUID(as_uuid=True), db.ForeignKey('datasets.id'), primary_key=True),
-    db.Column('file_id', UUID(as_uuid=True), db.ForeignKey('data_files.id'), primary_key=True)                               
-)
-
 class Datasets(db.Model):
     __tablename__ = 'datasets'
     id = db.Column(UUID(as_uuid=True), primary_key=True, server_default=sqlalchemy.text("gen_random_uuid()"))
@@ -25,9 +15,10 @@ class Datasets(db.Model):
     updated_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
     owner_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=True)
     tenancy = db.Column(db.String(256), nullable=True)
+    design_state = db.Column(db.String(64), nullable=True)
 
-    versions = relationship('DatasetVersions', secondary=dataset_version_association, backref='datasets')
-    files = relationship('DataFiles', secondary=dataset_files_association, backref='datasets')
+    versions = relationship('DatasetVersions', backref='dataset')
+    files = relationship('DataFiles', backref='dataset')
 
     __table_args__ = (Index('idx_is_enabled', 'is_enabled'),
                       Index('idx_name', 'name'))
@@ -39,9 +30,11 @@ class DatasetVersions(db.Model):
     description = db.Column(db.String(256), nullable=True)
     created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
-    author_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=True)
+    created_by = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=True)
     is_enabled = db.Column(db.Boolean, nullable=False, default=True)
-
+    dataset_id = db.Column(UUID(as_uuid=True), db.ForeignKey('datasets.id'), nullable=False)
+    design_state = db.Column(db.String(64), nullable=True)
+    
     __table_args__ = (Index('idx_dataset_versions_name', 'name'),
                       Index('idx_dataset_versions_created_at', 'created_at'))
 
@@ -56,7 +49,8 @@ class DataFiles(db.Model):
     storage_path = db.Column(db.String(2048), nullable=True)
     created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
-    author_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=False)
+    created_by = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=False)
+    version_id = db.Column(UUID(as_uuid=True), db.ForeignKey('dataset_versions.id'), nullable=False)
 
     __table_args__ = (Index('idx_data_files_name', 'name'),
                       Index('idx_data_files_created_at', 'created_at'))
