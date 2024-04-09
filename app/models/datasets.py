7682f9b09afa9f97c8ad78b6a0a1fbd2d30f4ffa
Caio Maia <caaiomaia@gmail.com>
2024-04-08 23:01:09 -0300

    Change design_state into enums

--- app/models/datasets.py
+++ app/models/datasets.py
@@ -1,9 +1,14 @@
 import sqlalchemy
 from app import db
 from sqlalchemy.dialects.postgresql import UUID, JSONB
-from sqlalchemy import Index
+from sqlalchemy import Index, Enum
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
+import enum
+
+class DesignState(enum.Enum):
+    DRAFT='draft',
+    PUBLISHED='published'
 
 class Datasets(db.Model):
     __tablename__ = 'datasets'
@@ -15,10 +20,9 @@ class Datasets(db.Model):
     updated_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
     owner_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=True)
     tenancy = db.Column(db.String(256), nullable=True)
-    design_state = db.Column(db.String(64), nullable=True)
+    design_state = db.Column(Enum(DesignState), nullable=True)
 
     versions = relationship('DatasetVersions', backref='dataset')
-    files = relationship('DataFiles', backref='dataset')
 
     __table_args__ = (Index('idx_is_enabled', 'is_enabled'),
                       Index('idx_name', 'name'))
@@ -32,9 +36,11 @@ class DatasetVersions(db.Model):
     updated_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
     created_by = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=True)
     is_enabled = db.Column(db.Boolean, nullable=False, default=True)
-    dataset_id = db.Column(UUID(as_uuid=True), db.ForeignKey('datasets.id'), nullable=False)
-    design_state = db.Column(db.String(64), nullable=True)
+    dataset_id = db.Column(UUID(as_uuid=True), db.ForeignKey('datasets.id'), nullable=True)
+    design_state = db.Column(Enum(DesignState), nullable=True)
     
+    files = relationship('DataFiles', backref='dataset_version')
+
     __table_args__ = (Index('idx_dataset_versions_name', 'name'),
                       Index('idx_dataset_versions_created_at', 'created_at'))
 
@@ -49,7 +55,7 @@ class DataFiles(db.Model):
     storage_path = db.Column(db.String(2048), nullable=True)
     created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
-    created_by = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=False)
+    created_by = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=True)
     version_id = db.Column(UUID(as_uuid=True), db.ForeignKey('dataset_versions.id'), nullable=False)
 
     __table_args__ = (Index('idx_data_files_name', 'name'),
