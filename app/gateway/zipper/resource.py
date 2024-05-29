fc6b2f83dd4807593b80c6f88dcb0ab4736632d2
Caio Maia <caaiomaia@gmail.com>
2024-05-28 22:05:49 -0300

    Create zipper gateway

---
+++ app/gateway/zipper/resource.py
@@ -0,0 +1,12 @@
+from uuid import UUID
+from pydantic import BaseModel, Field
+
+class CreateZipRequest(BaseModel):
+    files: list[str] = Field(..., title="List of file names to be zipped")
+    bucket: str = Field(..., title="Name of the bucket")
+    zip_name: str | None = Field(None, title="Name of the zip file")
+
+class CreateZipResponse(BaseModel):
+    id: UUID = Field(..., title="Zip ID")
+    status: str = Field(..., title="Zip status")
+    name: str | None = Field(None, title="Name of the zip file")
