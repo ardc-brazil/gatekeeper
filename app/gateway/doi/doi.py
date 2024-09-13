1fd5aff9596009ffbd2a697dc1669e74c056ae9e
Caio Maia <caaiomaia@gmail.com>
2024-09-13 17:42:55 -0300

    Create doi gateway

---
+++ app/gateway/doi/doi.py
@@ -0,0 +1,64 @@
+import dataclasses
+import requests
+
+from app.gateway.doi.resource import DOIPayload
+
+class DOIGateway:
+    def __init__(self, base_url: str, repository: str, login: str, password: str):
+        self._base_url = base_url
+        self._repository = repository
+        self._login = login
+        self._password = password
+        self._base_headers = {
+            "Content-Type": "application/vnd.api+json"
+        }
+    
+    def post(self, doi: DOIPayload) -> dict:
+        url = f"{self._base_url}/dois"
+        response = requests.post(
+            url, 
+            headers=self._base_headers, 
+            auth=(self._login, self._password),
+            json=dataclasses.asdict(doi))
+        
+        if not response.status_code == 201:
+            raise Exception(f"Error creating DOI: {response.text}")
+        
+        return response.json()
+
+    def get(self, identifier: str) -> dict:
+        url = f"{self._base_url}/dois/{self._repository}/{identifier}"
+        response = requests.get(
+            url, 
+            headers=self._base_headers, 
+            auth=(self._login, self._password))
+        
+        if not response.status_code == 200:
+            raise Exception(f"Error getting DOI: {response.text}")
+        
+        return response.json()
+    
+    def update(self, doi: DOIPayload, identifier: str) -> dict:
+        url = f"{self._base_url}/dois/{self._repository}/{identifier}"
+        response = requests.put(
+            url, 
+            headers=self._base_headers, 
+            auth=(self._login, self._password),
+            json=dataclasses.asdict(doi))
+        
+        if not response.status_code == 200:
+            raise Exception(f"Error updating DOI: {response.text}")
+        
+        return response.json()
+    
+    def delete(self, identifier: str) -> dict:
+        url = f"{self._base_url}/dois/{self._repository}/{identifier}"
+        response = requests.delete(
+            url, 
+            headers=self._base_headers, 
+            auth=(self._login, self._password))
+        
+        if not response.status_code == 204:
+            raise Exception(f"Error deleting DOI: {response.text}")
+        
+        return response.json()
---
+++ app/gateway/doi/resource.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+import enum
+from typing import List
+
+class Event(enum.Enum):
+    PUBLISH = "publish"
+    REGISTER = "register"
+
+@dataclass
+class Creator:
+    name: str
+
+@dataclass
+class Title:
+    title: str
+
+@dataclass
+class Types:
+    resourceTypeGeneral: str = "Dataset"
+
+@dataclass
+class Attributes:
+    prefix: str
+    creators: List[Creator]
+    titles: List[Title]
+    publisher: str
+    publicationYear: int
+    types: Types
+    url: str
+    # If None will create as "DRAFT"
+    event: str = None
+
+@dataclass
+class Data:
+    attributes: Attributes
+    type: str = "dois"
+
+@dataclass
+class DOIPayload:
+    data: Data
