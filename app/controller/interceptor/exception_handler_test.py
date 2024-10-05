4929a8d62fb9617f91c1b72ec3f60cfcbe585d62
André Maia <andrenmaia@gmail.com>
2024-10-05 12:21:40 -0300

    Add unit tests

---
+++ app/controller/interceptor/exception_handler_test.py
@@ -0,0 +1,26 @@
+import json
+import unittest
+
+from app.controller.interceptor.exception_handler import bad_request_exception_handler
+from app.exception.bad_request import BadRequestException, ErrorDetails
+
+class TestExceptionHandler(unittest.IsolatedAsyncioTestCase):
+    
+    async def test_bad_response(self):
+        # given
+        request = None
+        exception = BadRequestException(errors=[ErrorDetails(code="missing_field", field="field1" )])
+
+        # when
+        response = await bad_request_exception_handler(request=request,exc=exception)
+
+        # then
+        self.assertEqual(response.status_code, 400)
+        data = json.loads(response.body)
+        print(data)
+        self.assertEqual(data['details'], 'Invalid client input')
+        self.assertListEqual(data['errors'], [{'code':'missing_field', 'field': 'field1'}])
+
+
+if __name__ == "__main__":
+    unittest.main()
